236b1848990ca996b4aa00c57a6fe772a83c2c61
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-06-06 23:29:32 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,33 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("¡Netflix App funcionando!");
-        Scene scene = new Scene(label, 400, 200);
+    public void start(Stage stage) throws Exception {
+        // Cargar el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/main/app/views/MainView.fxml"));
+        Scene scene = new Scene(root);
+        // Depuración: verifica si el CSS existe
+        java.net.URL cssUrl = getClass().getResource("css/styles.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        } else {
+            System.err.println("No se encontró el archivo CSS.");
+        }
         stage.setScene(scene);
         stage.setTitle("Hola JavaFX");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args); // Ahora sí funcionará correctamente
+        launch(args);
+        
     }
 }
 
